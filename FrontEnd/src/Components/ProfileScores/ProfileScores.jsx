ba7be86462e0620a8d303958bebb8abfdff614a9
maryam-hussien <maryamhussien122@gmail.com>
2024-09-30 19:48:07 +0300

    profilescores

--- FrontEnd/src/Components/ProfileScores/ProfileScores.jsx
+++ FrontEnd/src/Components/ProfileScores/ProfileScores.jsx
@@ -1,20 +1,24 @@
+import "./profileScores.css"
 
 const ProfileScores = () => {
   return (
-    <div className="d-flex flex-wrap">ProfileScores Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio at commodi nisi blanditiis aperiam odio possimus laborum mollitia quod voluptatum cum cupiditate saepe amet laudantium, nobis quas nihil doloremque veritatis.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad neque consequuntur nobis odio quos. Doloribus illo ipsum corporis necessitatibus voluptas ad eius autem commodi totam illum, expedita vero cumque.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad neque consequuntur nobis odio quos. Doloribus illo ipsum corporis necessitatibus voluptas ad eius autem commodi totam illum, expedita vero cumque.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad neque consequuntur nobis odio quos. Doloribus illo ipsum corporis necessitatibus voluptas ad eius autem commodi totam illum, expedita vero cumque.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad neque consequuntur nobis odio quos. Doloribus illo ipsum corporis necessitatibus voluptas ad eius autem commodi totam illum, expedita vero cumque.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad neque consequuntur nobis odio quos. Doloribus illo ipsum corporis necessitatibus voluptas ad eius autem commodi totam illum, expedita vero cumque.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad neque consequuntur nobis odio quos. Doloribus illo ipsum corporis necessitatibus voluptas ad eius autem commodi totam illum, expedita vero cumque.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad neque consequuntur nobis odio quos. Doloribus illo ipsum corporis necessitatibus voluptas ad eius autem commodi totam illum, expedita vero cumque.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad neque consequuntur nobis odio quos. Doloribus illo ipsum corporis necessitatibus voluptas ad eius autem commodi totam illum, expedita vero cumque.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad neque consequuntur nobis odio quos. Doloribus illo ipsum corporis necessitatibus voluptas ad eius autem commodi totam illum, expedita vero cumque.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad neque consequuntur nobis odio quos. Doloribus illo ipsum corporis necessitatibus voluptas ad eius autem commodi totam illum, expedita vero cumque.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad neque consequuntur nobis odio quos. Doloribus illo ipsum corporis necessitatibus voluptas ad eius autem commodi totam illum, expedita vero cumque.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad neque consequuntur nobis odio quos. Doloribus illo ipsum corporis necessitatibus voluptas ad eius autem commodi totam illum, expedita vero cumque.
- </div>
+    <div className="d-flex flex-wrap flex-column text-capitalize ProfileScores container mt-5 ">
+      <h3 className="text-center ">Student grades</h3>
+     <div className="gradeSection border border-secondary rounded-2 d-flex flex-column mx-auto my-2">
+      <div className="fw-semibold text-secondary">2024-06-21</div>
+      <div className="grades d-flex fw-bold">
+        <div className="p-1">midterm in science</div>
+        <div className="p-1">0/100</div>
+      </div>
+    </div> 
+     <div className="gradeSectionn border border-secondary rounded-2 d-flex flex-column mx-auto my-1">
+      <div className="fw-semibold text-secondary">2024-06-21</div>
+      <div className="grades d-flex fw-bold">
+        <div className="p-1">final in science</div>
+        <div className="p-1">0/100</div>
+      </div>
+    </div> 
+    </div>
   )
 }
 
