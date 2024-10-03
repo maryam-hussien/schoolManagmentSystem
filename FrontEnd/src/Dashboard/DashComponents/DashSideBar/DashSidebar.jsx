b0e7171779ed41be4a7341d1bb820e5608a0f5c1
maryam-hussien <maryamhussien122@gmail.com>
2024-10-04 02:46:39 +0300

    edits

--- FrontEnd/src/Dashboard/DashComponents/DashSideBar/DashSidebar.jsx
+++ FrontEnd/src/Dashboard/DashComponents/DashSideBar/DashSidebar.jsx
@@ -13,31 +13,35 @@ const DashSidebar = () => {
         <ul className="nav flex-column m-auto">
           <a className="icon-link icon-link-hover link-opacity-50-hover link-light fw-light mb-4" href="#">
             <Boxes style={{ color: 'white', fontSize: '19px' }} />
-            Dashboard
+            <span>Dashboard</span>
           </a>
           <a className="icon-link icon-link-hover link-opacity-50-hover link-light fw-light mb-4" href="#">
             <PersonFill style={{ color: 'white', fontSize: '19px' }} />
-            Profile
+            <span>Profile</span>
           </a>
           <a className="icon-link icon-link-hover link-opacity-50-hover link-light fw-light mb-4" href="#">
             <Book style={{ color: 'white', fontSize: '19px' }} />
-            Courses
+            <span>courses</span>
+            
           </a>
           <a className="icon-link icon-link-hover link-opacity-50-hover link-light fw-light mb-4" href="#">
             <CalendarCheckFill style={{ color: 'white', fontSize: '19px' }} />
-            Attendence
+            <span>Attendence</span>
+
           </a>
           <a className="icon-link icon-link-hover link-opacity-50-hover link-light fw-light mb-4" href="#">
             <ClipboardData style={{ color: 'white', fontSize: '19px' }} />
-            Grades
+            <span>Grades</span>
+
           </a>
           <a className="icon-link icon-link-hover link-opacity-50-hover link-light fw-light mb-4" href="#">
             <Grid3x3GapFill style={{ color: 'white', fontSize: '19px' }} />
-            Schedule
+            <span>Schedule</span>
+
           </a>
           <a className="icon-link icon-link-hover link-opacity-50-hover  fw-light mb-4 opacity-100" href="#" style={{color:'red'}}>
             <ArrowBarLeft style={{ color: 'red', fontSize: '19px' }} />
-            Log Out
+            <span>Log Out</span>
           </a>
 
 
