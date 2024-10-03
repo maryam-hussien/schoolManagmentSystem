dd7b6c2593780e5d9c9404e54fcddd2a89cd0b03
maryam-hussien <maryamhussien122@gmail.com>
2024-10-03 05:48:17 +0300

    edits

--- FrontEnd/src/pages/Home/Home.jsx
+++ FrontEnd/src/pages/Home/Home.jsx
@@ -72,22 +72,22 @@ const Home = () => {
           <h2>Smarty Programs</h2>
           <div className="program-cards">
             <div className="program-card">
-              <FaBabyCarriage className="program-icon" />
+              <FaBabyCarriage className="program-icon" style={{color:'fd7899'}} />
               <h3>Kindergarten</h3>
               <p>Creating a safe and welcoming environment for learning.</p>
             </div>
             <div className="program-card">
-              <FaChalkboardTeacher className="program-icon" />
+              <FaChalkboardTeacher className="program-icon" style={{color:'rgb(255 209 98)'}} />
               <h3>Primary School</h3>
               <p>Encouraging exploration and curiosity through diverse activities.</p>
             </div>
             <div className="program-card">
-              <FaSchool className="program-icon" />
+              <FaSchool className="program-icon"style={{color:'rgb(111 228 211);'}} />
               <h3>Secondary School</h3>
               <p>Preparing students for future academic challenges and life skills.</p>
             </div>
             <div className="program-card">
-              <FaUserGraduate className="program-icon" />
+              <FaUserGraduate className="program-icon" style={{color:'rgb(74 152 159)'}} />
               <h3>Preparatory School</h3>
               <p>Fostering essential skills to excel in the next academic level.</p>
             </div>
