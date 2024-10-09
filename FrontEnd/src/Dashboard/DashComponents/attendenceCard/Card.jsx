3700188281caa96c0810bee97a3d771a97f2407e
maryam-hussien <maryamhussien122@gmail.com>
2024-10-09 19:04:54 +0300

    dash attendence

--- FrontEnd/src/Dashboard/DashComponents/attendenceCard/Card.jsx
+++ FrontEnd/src/Dashboard/DashComponents/attendenceCard/Card.jsx
@@ -1,9 +1,22 @@
-import './card.css'
-
+import "./card.css";
+ 
 const Card = () => {
   return (
-    <div className='attendcard fs-5 fw-light'>card</div>
-  )
-}
+    <div className="attendcard fs-5 fw-light">
+      <div className="date">21-6-2024</div>
+      <div className="mainInfo">
+        <img></img>
+        <div>
+          <div> maryam hussien </div>
+          <div className="id">2027222 </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-around m-auto flex-wrap">
+        <button className="btn attendcardbtn">Present</button>
+        <button className="btn">Absent</button>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
