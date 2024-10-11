6d0308960b3f9b5da34b0488ea34e0744c42af88
menna544 <mennamaged121@gmail.com>
2024-10-11 21:22:27 +0200

    commit 4

--- FrontEnd/src/Components/courses/Bar.jsx
+++ FrontEnd/src/Components/courses/Bar.jsx
@@ -6,7 +6,7 @@ import courses from '../../../public/data/coursresapi';
 
 const Bar = () => {
   const [filter, setFilter] = useState('');
-  const [selectedSection, setSelectedSection] = useState(''); // New state to track selected section
+  const [selectedSection, setSelectedSection] = useState('');
 
   const handleFilterChange = (newFilter) => {
     setFilter(newFilter);
@@ -17,7 +17,7 @@ const Bar = () => {
     : [];
 
   const isPrimary = window.location.pathname.includes('primary');
-  const isKindergarten = window.location.pathname.includes('kindergarten');
+  const isKindergarten = window.location.pathname.includes('kindergaten');
   const isSecondaryOrPreparatory = window.location.pathname.includes('secondary') || window.location.pathname.includes('preparatory');
 
   return (
@@ -34,7 +34,7 @@ const Bar = () => {
         <NavLink to='kindergaten'>
           <button 
             className="btn btn-outline-primary"
-            onClick={() => setSelectedSection('kindergarten')} // Set section to "kindergarten"
+            onClick={() => setSelectedSection('kindergarten')} 
           >
             Kindergarten
           </button>
@@ -42,7 +42,7 @@ const Bar = () => {
         <NavLink to='primary'>
           <button 
             className="btn btn-outline-primary"
-            onClick={() => setSelectedSection('primary')} // Set section to "primary"
+            onClick={() => setSelectedSection('primary')} 
           >
             Primary
           </button>
@@ -50,7 +50,7 @@ const Bar = () => {
         <NavLink to='preparatory'>
           <button 
             className="btn btn-outline-primary"
-            onClick={() => setSelectedSection('preparatory')} // Set section to "preparatory"
+            onClick={() => setSelectedSection('preparatory')} 
           >
             Preparatory
           </button>
@@ -58,13 +58,12 @@ const Bar = () => {
         <NavLink to='secondary'>
           <button 
             className="btn btn-outline-primary"
-            onClick={() => setSelectedSection('secondary')} // Set section to "secondary"
+            onClick={() => setSelectedSection('secondary')} 
           >
             Secondary
           </button>
         </NavLink>
 
-        {/* Conditionally render FilterButton only for sections other than "myCourse" */}
         {(selectedSection === 'primary' || 
           selectedSection === 'kindergarten' || 
           selectedSection === 'secondary' || 
@@ -79,8 +78,8 @@ const Bar = () => {
       </section>
 
       <section className='courses'>
-        <div className="courseContainer mb-5 py-3 px-2">
-          <h2 className="text-center fw-medium">Filtered Courses ({filter || 'None'})</h2>
+        <div className="courseContainer ">
+        
           <div className='course-list'>
             {filteredCourses.length > 0 ? (
               filteredCourses.map(course => (
