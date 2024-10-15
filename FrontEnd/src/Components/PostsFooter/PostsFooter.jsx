61f3c8157c3ad2d6cadd16e4cbdf754c9f5b8861
maryam-hussien <maryamhussien122@gmail.com>
2024-10-16 01:53:34 +0300

    routes for social community

--- FrontEnd/src/Components/PostsFooter/PostsFooter.jsx
+++ FrontEnd/src/Components/PostsFooter/PostsFooter.jsx
@@ -1,27 +1,41 @@
-import './PostsFooter.css'
-import { PatchExclamationFill , PostcardHeartFill, ChatTextFill  } from 'react-bootstrap-icons';
-import Back from '../../Dashboard/DashComponents/BackButton/Back';
+import './PostsFooter.css';
+import { PatchExclamationFill, PostcardHeartFill, ChatTextFill } from 'react-bootstrap-icons';
+import { NavLink } from 'react-router-dom';
 
 const PostsFooter = () => {
   return (
-    <>
     <div className='PostsFooter'>
-           <div className="d-flex align-items-center PostsFoot ">
-                <Back link='/social' color='rgb(130 0 130)' font='20px' Icon={PostcardHeartFill} label='POSTS' labelColor='black' labelFontWeight='400'/>
-              </div>
-           <div className="d-flex align-items-center PostsFoot ">
-           <Back link='/social/reviews' color='rgb(233 52 83)' font='20px' Icon={PatchExclamationFill} label='Reviews' labelColor='black' labelFontWeight='400'/>
-              </div>
-           <div className="d-flex align-items-center PostsFoot ">
-                <Back link='/social/chats' color='#045804' font='20px' Icon={ChatTextFill} label='Chats' labelColor='black' labelFontWeight='400'/>
-
-              </div>
-
-          </div>
-
-</>
-  
-  )
+      {/* Posts Link */}
+      <NavLink 
+        to='/social' 
+        className="d-flex align-items-center PostsFoot text-decoration-none" 
+        activeClassName="active-link"
+      >
+        <PostcardHeartFill style={{ color: 'rgb(130 0 130)', fontSize: '20px' }} />
+        <span className="ms-2" style={{ color: 'black', fontWeight: '400' }}>POSTS</span>
+      </NavLink>
+      
+      {/* Reviews Link */}
+      <NavLink 
+        to='/social/reviews' 
+        className="d-flex align-items-center PostsFoot text-decoration-none" 
+        activeClassName="active-link"
+      >
+        <PatchExclamationFill style={{ color: 'rgb(233 52 83)', fontSize: '20px' }} />
+        <span className="ms-2" style={{ color: 'black', fontWeight: '400' }}>Reviews</span>
+      </NavLink>
+      
+      {/* Chats Link */}
+      <NavLink 
+        to='/social/chats' 
+        className="d-flex align-items-center PostsFoot text-decoration-none" 
+        activeClassName="active-link"
+      >
+        <ChatTextFill style={{ color: '#045804', fontSize: '20px' }} />
+        <span className="ms-2" style={{ color: 'black', fontWeight: '400' }}>Chats</span>
+      </NavLink>
+    </div>
+  );
 }
 
-export default PostsFooter
+export default PostsFooter;
--- FrontEnd/src/pages/Social/Social.jsx
+++ FrontEnd/src/pages/Social/Social.jsx
@@ -1,42 +1,23 @@
 import  { useState } from 'react';
-import AddPost from "../../Components/AddPost/AddPost"; // Ensure you have this import
 import SocialNav from "../../Components/SocailNav/SocialNav";
-import postsData from "../../../public/data/socialPostsapi"; // Ensure this path is correct
+import { Route, Routes } from "react-router-dom";
 import "./Social.css";
-import PostCard from '../../Components/PostCard/PostCard';
-import img from "../../../public/assets/Connected world.svg"
 import PostsFooter from '../../Components/PostsFooter/PostsFooter';
+import Post from '../../Components/PostPage/Post';
 
 const Social = () => {
-  const [posts, setPosts] = useState(postsData); // Initialize state with imported posts data
 
-  // Handler function to add a new post
-  const handleAddPost = (newPost) => {
-    setPosts((prevPosts) => [...prevPosts, newPost]); // Update the posts state with the new post
-  };
 
   return (
     <>
       <SocialNav />
       <div className="Social p-3">
         <div className="SocialTitle">School Social Community</div>
-        <div className='d-flex flex-wrap  align-items-start px-3 SocialDiv'>
-          <img src={img} className='SocialImg' alt="Social" />
-          <AddPost onAddPost={handleAddPost} /> {/* Pass the function to AddPost */}
-        </div>
-        <div className='SocialTitle text-center mt-2'>POSTS</div>
-        {/* Display the list of posts */}
-        <div >
-          {posts.length === 0 ? (
-            <p>No posts yet.</p>
-          ) : (
-            <ul className="list-group">
-              {posts.map((post) => (
-                <PostCard key={post.id} post={post} />
-              ))}
-            </ul>
-          )}
-        </div>
+        <Routes>
+              <Route path="/" element={<Post />}/>
+
+
+            </Routes>
       </div>
       <PostsFooter/>
     </>
