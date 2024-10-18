296114ad34b8657d7fb1637d510ec8a45cbed2fc
mariemmohamed374 <marimmemo849@gmail.com>
2024-10-18 04:56:24 +0300

    add dashStudent page

--- FrontEnd/src/Dashboard/DashPages/DashStudents/Students.jsx
+++ FrontEnd/src/Dashboard/DashPages/DashStudents/Students.jsx
@@ -1,11 +1,25 @@
-import './Students.css'
 
-const Students = () => {
+import CoursesCard from "../../DashComponents/courseCard/CoursesCard";
+import GradesCard from "../../DashComponents/GradesCard/GradesCard";
+import AnnouncementsCard from "../../DashComponents/AnnouncementsCard/AnnouncementsCard";
+import AbsentDaysCard from '../../DashComponents/AbsentDaysCard/AbsentDaysCard'
+import './Students.css';
+
+const Student = () => {
   return (
-    <div>
-      Students
-    </div>
-  )
-}
+    <div className="studentdashboard-container">
 
-export default Students
+
+        <div className="cards-container">
+          <CoursesCard />
+          <GradesCard />
+          <AnnouncementsCard />
+          <AbsentDaysCard />
+        </div>
+    
+    </div>
+  );
+};
+
+export default Student;
+
