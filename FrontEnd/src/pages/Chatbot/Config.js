a0b83f991bae1200d303d17d158b53c9e01fb903
maryam-hussien <maryamhussien122@gmail.com>
2025-01-02 04:04:19 +0200

    remove src folder

--- FrontEnd/src/pages/Chatbot/Config.js
+++
@@ -1,8 +0,0 @@
-const Config = {
-    botName: "Chatbot",
-    initialMessages: [{ text: "Hello! How can I assist you today?", id: 1 }],
-    // Add more configuration options as needed
-  };
-  
-  export default Config;
-  
